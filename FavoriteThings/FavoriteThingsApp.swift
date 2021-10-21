7d9ecb0ec98e27ff5c26dabacd5c49b78afdc8c9
Joyce Tao <jtao@lcs.on.ca>
2021-10-21 14:56:56 -0400

    Initial Commit

---
+++ FavoriteThings/FavoriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteThingsApp.swift
+//  FavoriteThings
+//
+//  Created by Joyce Tao on 2021-10-21.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
