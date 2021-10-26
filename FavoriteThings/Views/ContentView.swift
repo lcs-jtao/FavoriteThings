43cc95da7fb47aa6fff7f1e5f37b11b38ffe1139
Joyce Tao <jtao@lcs.on.ca>
2021-10-26 11:20:02 -0400

    Added scrolling to each page

--- FavoriteThings/Views/ContentView.swift
+++ FavoriteThings/Views/ContentView.swift
@@ -11,16 +11,18 @@ struct ContentView: View {
     
     // "body" is a computed property
     var body: some View {
-        VStack {
-            // opaque returned type
-            Image("Spongecake")
-                .resizable()
-                .scaledToFit()
-            
-            Text("This is a picture of my adorable four-year-old little dog learning on my pillow. Her name is Spongecake and she is a bichon frise :D")
-                .padding()
-            
-            Spacer()
+        ScrollView {
+            VStack {
+                // opaque returned type
+                Image("Spongecake")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("This is a picture of my adorable four-year-old little dog learning on my pillow. Her name is Spongecake and she is a bichon frise :D")
+                    .padding()
+                
+                Spacer()
+            }
         }
         .navigationTitle("Spongecake")
     }
--- FavoriteThings/Views/SunsetView.swift
+++ FavoriteThings/Views/SunsetView.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 struct SunsetView: View {
     var body: some View {
-        VStack {
-            Image("Sunset")
-                .resizable()
-                .scaledToFit()
-            
-            Text("""
-                 This photo was taken at 6：11 pm on October 18, 2021.
-                 
-                 I saw this beautiful sunset scene and took a photo of it when I went down to the waterfront of Lakefield College School with my friend.
-                 """)
-                .padding()
-            
-            Spacer()
+        ScrollView {
+            VStack {
+                Image("Sunset")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("""
+                     This photo was taken at 6：11 pm on October 18, 2021.
+                     
+                     I saw this beautiful sunset scene and took a photo of it when I went down to the waterfront of Lakefield College School with my friend.
+                     """)
+                    .padding()
+                
+                Spacer()
+            }
         }
         .navigationTitle("Sunset")
     }
